1b4a8953373e4ef8b3b9f29ac5d9559b983332f4
7H0shi <fernandoardiansyah6@gmail.com>
2025-06-01 19:12:25 +0700

    gacorbot

--- src/Tubes1_9ame 9acor/run-bots.sh
+++ src/Tubes1_9ame 9acor/run-bots.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python main.py --logic gacorbot --email=test2@email.com --name=cipa --password=333333 --team etimom &
+python main.py --logic gacorbot --email=9ame_9acor@email.com --name=gacorbot --password=123456 --team etimo &
